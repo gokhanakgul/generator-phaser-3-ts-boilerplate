7432bcc00e22a133a6dc35bb7303118a73ff554b
gokhanakgul <gokhan.akgul@gmail.com>
2018-10-19 16:13:03 +0300

    Fix some formatting issues

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const path = require('path');
 const chalk = require('chalk');
 const yosay = require('yosay');
@@ -9,47 +10,40 @@ module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log(
-      yosay(`Welcome to the good ${chalk.red('generator-phaser-3-ts-boilerplate')} generator!`)
+      yosay(
+        `Welcome to the good ${chalk.red('generator-phaser-3-ts-boilerplate')} generator!`
+      )
     );
 
-
-    return this.prompt([{
+    return this.prompt([
+      {
         type: 'input',
         name: 'projectName',
-        message: 'What\'s the name of your application(in English Please)',
+        message: "What's the name of your application(in English Please)",
         default: foldername
       },
       {
         type: 'input',
         name: 'projectDesc',
-        message: 'What\'s the description of your application(optional):'
+        message: "What's the description of your application(optional):"
       },
       {
         type: 'input',
         name: 'projectAuthor',
-        message: 'Who\'s the author of your application(optional):',
+        message: "Who's the author of your application(optional):"
       },
       {
         type: 'input',
         name: 'projectAuthorEmail',
-        message: 'What\'s the author\'s email of your application(optional):',
+        message: "What's the author's email of your application(optional):"
       }
-
-      /*
-      ,{
-        type: 'list',
-        name: 'projectLicense',
-        message: 'Please choose license:',
-        choices: ['MIT', 'Apache-2.0', 'GPL-3.0', 'Others'],
-        default: 'MIT'
-      }*/
-
     ]).then(answers => {
       this.projectName = answers.projectName ? answers.projectName : foldername;
       this.projectDesc = answers.projectDesc ? answers.projectDesc : '';
       this.projectAuthor = answers.projectAuthor ? answers.projectAuthor : '';
-      this.projectAuthorEmail = answers.projectAuthorEmail ? answers.projectAuthorEmail : '';
-      // this.projectLicense = answers.projectLicense ? (answers.projectLicense === 'Others' ? '' : answers.projectLicense) : 'MIT';
+      this.projectAuthorEmail = answers.projectAuthorEmail
+        ? answers.projectAuthorEmail
+        : '';
     });
   }
 
@@ -60,50 +54,25 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath('assets'),
-      this.destinationPath('assets'),
-      this
-    );
+    this.fs.copy(this.templatePath('assets'), this.destinationPath('assets'), this);
 
-    this.fs.copy(
-      this.templatePath('src'),
-      this.destinationPath('src'),
-      this
-    );
+    this.fs.copy(this.templatePath('src'), this.destinationPath('src'), this);
 
-    this.fs.copyTpl(
-      this.templatePath('_bower.json'),
-      this.destinationPath('bower.json'),
-      this
-    );
-    this.fs.copyTpl(
-      this.templatePath('_index.html'),
-      this.destinationPath('index.html'),
-      this
-    );
+    this.fs.copyTpl(this.templatePath('_bower.json'), this.destinationPath('bower.json'));
+    this.fs.copyTpl(this.templatePath('_index.html'), this.destinationPath('index.html'));
     this.fs.copyTpl(
       this.templatePath('_package.json'),
-      this.destinationPath('package.json'),
-      this
+      this.destinationPath('package.json')
     );
-    this.fs.copyTpl(
-      this.templatePath('_README.md'),
-      this.destinationPath('README.md'),
-      this
-    );
+    this.fs.copyTpl(this.templatePath('_README.md'), this.destinationPath('README.md'));
     this.fs.copy(
       this.templatePath('tsconfig.json'),
-      this.destinationPath('tsconfig.json'),
-      this
+      this.destinationPath('tsconfig.json')
     );
     this.fs.copy(
       this.templatePath('webpack.config.js'),
-      this.destinationPath('webpack.config.js'),
-      this
+      this.destinationPath('webpack.config.js')
     );
-
-    //this.fs.copy(this.sourceRoot(), this.destinationRoot());
   }
 
   install() {
