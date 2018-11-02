8dc949235b81373e5f6fb9664a78313af9d80a57
gokhanakgul <gokhan.akgul@gmail.com>
2018-11-02 13:16:18 +0300

    updated test scenarios

--- __tests__/app.js
+++ __tests__/app.js
@@ -3,7 +3,8 @@ const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
-describe('generator-phaser-3-ts-boilerplate:app', () => {
+/*
+Describe('generator-phaser-3-ts-boilerplate:app', () => {
   beforeAll(() => {
     return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({
       projectName: 'testProject',
@@ -15,5 +16,23 @@ describe('generator-phaser-3-ts-boilerplate:app', () => {
 
   it('creates files', () => {
     assert.file(['.gitignore']);
+  });
+});
+*/
+
+describe('generator-phaser-3-ts-boilerplate:app', () => {
+  it('generate a project', function() {
+    // The object returned acts like a promise, so return it to wait until the process is done
+    return helpers
+      .run(path.join(__dirname, '../app'))
+      .withPrompts({
+        projectName: 'testProject',
+        projectDesc: 'testProject Description',
+        projectAuthor: 'Test Developer',
+        projectAuthorEmail: 'test@Developer.com'
+      })
+      .then(function() {
+        assert.file(['.gitignore']);
+      });
   });
 });
